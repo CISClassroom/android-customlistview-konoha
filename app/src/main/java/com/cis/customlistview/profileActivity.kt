4905271c7a4964ce8bf820f8c7103cfa2da05103
sasikorn-dream <sasikorn.k@github.com>
2020-01-03 11:43:07 +0700

    LabCustomListView

---
+++ app/src/main/java/com/cis/customlistview/profileActivity.kt
@@ -0,0 +1,16 @@
+package com.cis.customlistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class profileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        //get data from activity
+        val data = intent.getStringExtra("selectedname")
+        Toast.makeText(this,data, Toast.LENGTH_SHORT).show()
+    }
+}
