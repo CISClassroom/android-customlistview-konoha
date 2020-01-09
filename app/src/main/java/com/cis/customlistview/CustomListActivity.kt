579b04ac9f24bfda821e131a591cedd546d06926
TanyasiriP <33540463+TanyasiriP@users.noreply.github.com>
2020-01-10 01:07:05 +0700

    add some change

--- app/src/main/java/com/cis/customlistview/CustomListActivity.kt
+++ app/src/main/java/com/cis/customlistview/CustomListActivity.kt
@@ -8,5 +8,7 @@ class CustomListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_list)
+
+        
     }
 }
